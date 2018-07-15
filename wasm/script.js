9c895f667cf296503c444d3174ba00fd92874fb4
mixsistemasdev <mix.sistemas.dev@gmail.com>
2018-07-15 10:40:10 -0300

    js square sample

--- wasm/script.js
+++ wasm/script.js
@@ -7,9 +7,15 @@ function loadWasm(filename) {
         .then(module => { return new WebAssembly.Instance(module) });
 }
 
+/*
+    function square(num) {
+        return num * num;
+    }
+*/
+
 loadWasm('square.wasm') // http://mbebenita.github.io/WasmExplorer/
     .then(instance => {
         square = instance.exports._Z6squarei;
-        console.log(`square(5) = ${square(5)}`)
+        console.log(`square(5) = ${square(5)}`);
+        console.log('square(5) = ' + square(5));
     });
-
